2f3d7ae1b4c1a4a34f94d200488e7522d3806448
Sanjay Jaras <sanjayjaras@gmail.com>
2019-09-02 10:55:38 -0500

    Create Assignment2-1.py

---
+++ JARAS_DSC510/Assignment2-1.py
@@ -0,0 +1,25 @@
+'''
+    This program will show welcome message to user and ask for company name,
+    and take the number of feet fiber optics cable he wants to install.
+    Calculate the installation cost by multiplying by 0.87.
+
+    Author: Sanjay Jaras
+    Assignment: 2.1
+'''
+# print welcome message
+print("Welcome !!!")
+# get company name from user
+company_name = input("Please enter company name:")
+# get number of feet
+feet = input("Please enter the cable in length in feet:")
+rate_per_feet = 0.87
+installtion_cost = float(feet) * rate_per_feet
+line_length = 100
+print("\n", "*" * line_length, end="\n\n")
+print(" " * ((line_length // 2) - len(company_name)), company_name, end="\n\n")
+print("*" * line_length, end="\n\n")
+print("Cable length:\t", feet, end="\t" * 12)
+print("Installation cost per feet:\t", rate_per_feet, "$", end="\n\n")
+print("Calculated cost:", installtion_cost, "$", end="\t" * 11)
+print("Total cost:", "\t" * 3, installtion_cost, "$", end="\n\n")
+print("*" * line_length)
