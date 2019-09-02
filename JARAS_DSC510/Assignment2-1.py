678a5be92df7e8900376dcd2bba55b289446eec1
Sanjay Jaras <sanjayjaras@gmail.com>
2019-09-02 10:59:25 -0500

    Assignment 2.1

--- JARAS_DSC510/Assignment2-1.py
+++ JARAS_DSC510/Assignment2-1.py
@@ -13,13 +13,15 @@ company_name = input("Please enter company name:")
 # get number of feet
 feet = input("Please enter the cable in length in feet:")
 rate_per_feet = 0.87
-installtion_cost = float(feet) * rate_per_feet
+# Calculate installation cost
+installation_cost = float(feet) * rate_per_feet
 line_length = 100
+# Print result
 print("\n", "*" * line_length, end="\n\n")
 print(" " * ((line_length // 2) - len(company_name)), company_name, end="\n\n")
 print("*" * line_length, end="\n\n")
 print("Cable length:\t", feet, end="\t" * 12)
 print("Installation cost per feet:\t", rate_per_feet, "$", end="\n\n")
-print("Calculated cost:", installtion_cost, "$", end="\t" * 11)
-print("Total cost:", "\t" * 3, installtion_cost, "$", end="\n\n")
+print("Calculated cost:", installation_cost, "$", end="\t" * 11)
+print("Total cost:", "\t" * 3, installation_cost, "$", end="\n\n")
 print("*" * line_length)
