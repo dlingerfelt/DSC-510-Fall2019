8b6b588ed2d923f1194f43bfaecfa2d46b22185c
safarie1103 <54446804+safarie1103@users.noreply.github.com>
2019-09-13 14:03:43 -0500

    updates

--- Safari_Edris_DSC510/Week3/Safari_DSC510_Cable_Cost.py
+++ Safari_Edris_DSC510/Week3/Safari_DSC510_Cable_Cost.py
@@ -5,20 +5,25 @@
 # Desc: Get name of company and length in feet of fiber cable. compute cost at $.87 per foot. display result in recipt format.
 # Usage: Provide input when prompted.
 
+# Welcome Screen
 print("Welcome to Fiber Optics One.")
+# Get Company name
 print("Please tell us the name of your company")
 Company_Name = input()
 
 print("Thank You.")
 print("Now, please tell us the length in feet of cable you need. Enter 'q' to exit.")
-Cable_Length = input()
+Cable_Length = input('>>>')
+# Use while to run program until user enters 'q'
 while Cable_Length.lower() != 'q':
-    if not Cable_Length.isnumeric():
-        print("PLease enter a a valid number.")
+    if not Cable_Length.isnumeric():  # Make sure value is numeric
+        print("PLease enter a valid number.")
     else:
+        # Compute Installation cost at .87 cents per foot
         Installation_Cost = float(Cable_Length) * 0.87
-        Tax = Installation_Cost*.083;
+        Tax = Installation_Cost*.083  # Tax rate is 8.3%
         Installation_Cost_PlusTax = Installation_Cost + Tax
+        # Print out the receipt
         print("Thank You. Here's your receipt.")
         print("Company Name: " + Company_Name)
         print("Cable Length: " + Cable_Length)
@@ -27,5 +32,6 @@ while Cable_Length.lower() != 'q':
         print("Total cost for " + Cable_Length + " feet of cable at $.87 per foot is $" + str(format(Installation_Cost_PlusTax, '.2f')) + ".")
 
     print("Please enter another length in feet of cable you need. Enter 'q' to exit.")
-    Cable_Length = input()
+    Cable_Length = input('>>>')
+# Exit the program
 print("Thank You. Please come back.")
