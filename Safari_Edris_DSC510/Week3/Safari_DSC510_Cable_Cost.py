29e0cdce72509879f1d7dc8af50f638c4c7fd82e
safarie1103 <54446804+safarie1103@users.noreply.github.com>
2019-09-13 12:15:27 -0500

    updates to week3

--- Safari_Edris_DSC510/Week3/Safari_DSC510_Cable_Cost.py
+++ Safari_Edris_DSC510/Week3/Safari_DSC510_Cable_Cost.py
@@ -10,14 +10,22 @@ print("Please tell us the name of your company")
 Company_Name = input()
 
 print("Thank You.")
-print("Now, please tell us the length in feet of cable you need")
+print("Now, please tell us the length in feet of cable you need. Enter 'q' to exit.")
 Cable_Length = input()
-while Cable_Length > 0 :
+while Cable_Length.lower() != 'q':
     if not Cable_Length.isnumeric():
         print("PLease enter a a valid number.")
     else:
         Installation_Cost = float(Cable_Length) * 0.87
-    print("Thank You. Here's your receipt.")
-    print("Company Name: " + Company_Name)
-    print("Cable Length: " + Cable_Length)
-    print("Total cost for " + Cable_Length + " feet of cable at $.87 per foot is $" + str(Installation_Cost) + ".")
+        Tax = Installation_Cost*.083;
+        Installation_Cost_PlusTax = Installation_Cost + Tax
+        print("Thank You. Here's your receipt.")
+        print("Company Name: " + Company_Name)
+        print("Cable Length: " + Cable_Length)
+        print("Cost for " + Cable_Length + " feet of cable at $.87 per foot is $" + str(format(Installation_Cost, '.2f')) + ".")
+        print("Tax at 8.3% is $: " + str(format(Tax, '.2f')) + ".")
+        print("Total cost for " + Cable_Length + " feet of cable at $.87 per foot is $" + str(format(Installation_Cost_PlusTax, '.2f')) + ".")
+
+    print("Please enter another length in feet of cable you need. Enter 'q' to exit.")
+    Cable_Length = input()
+print("Thank You. Please come back.")
