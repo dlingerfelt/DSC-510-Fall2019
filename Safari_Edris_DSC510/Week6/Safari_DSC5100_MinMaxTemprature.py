ad7b7f6216597ca0d1de2daea5f9cf5a13637bb7
safarie1103 <54446804+safarie1103@users.noreply.github.com>
2019-10-06 08:26:13 -0500

    Update Safari_DSC5100_MinMaxTemprature.py

--- Safari_Edris_DSC510/Week6/Safari_DSC5100_MinMaxTemprature.py
+++ Safari_Edris_DSC510/Week6/Safari_DSC5100_MinMaxTemprature.py
@@ -5,12 +5,65 @@
 # Desc:
 # This program will populate a list of temperatures from user input.
 # This program will display the number of temperatures entered by user,
-# the maximum temperature entered and the minimum temperature entered by the user.# Usage: Provide input when prompted.
+# This program displays the maximum temperature and minimum temperature from the list entered by the user.
+# Usage: Provide input when prompted.
 
-#   Some global variables
+# Some global variables
 # List of temperatures is initialized here
 temperatures = []
 
+def isnegative(number):
+    # This function determines if given input is a negative number
+    # Return value is initialized to flse
+    retval = False
+    # Try & except are used to catch error if value given is string
+    try:
+        # Float function will fail if value is not numeric
+        if float(number) < 0:
+            retval = True
+        else:
+            rretval = false
+    except:
+        retval = False
+    return retval
+
+
+def getmax():
+    # This function returns maximum value in the list between integers and floats.
+    # This line gets maximum integer number
+    max_num = max(i for i in temperatures if isinstance(i, int))
+    # This line gets maximum float number
+    max_float = max(i for i in temperatures if isinstance(i, float))
+
+    # This conditional returns the higher value between int and float
+    if max_float > max_num:
+        return max_float
+    else:
+        return max_num
+
+def getmin():
+    # This function returns minimum value in the list between integers and floats.
+    # This line gets minimum integer number
+    min_num = min(i for i in temperatures if isinstance(i, int))
+    # This line gets minimum integer number
+    min_float = min(i for i in temperatures if isinstance(i, float))
+
+    # This conditional returns the lesser value between int and float
+    if min_float < min_num:
+        return min_float
+    else:
+        return min_num
+
+def isfloat(value):
+    # This function return tru is the given value is float.
+    # If the value give has one dot '.' in it, it is considered a float.
+    retval = False
+    numbers = str(value).split(".")
+    # This makes sure it's a float with only one '.'
+    if len(numbers) == 2:
+        retval = True
+
+    return retval
 
 
 def welcome_screen():
@@ -19,6 +72,7 @@ def welcome_screen():
     print("-------------------------")
     print("Welcome to Python Temperature display  .")
     print("-------------------------")
+
 
 welcome_screen()
 
@@ -30,24 +84,32 @@ while temperature.lower() != 'f':
     temperature = input('Please enter a temperature value (Enter \'f\' when done. ) >>> ')
 
     # Make sure user enters numeric value. they can enter f to end entering numbers.
-    while not temperature.isnumeric() and temperature.lower() != 'f':
+    while temperature.lower() != 'f' and not (temperature.isnumeric() or isfloat(temperature) or isnegative(temperature)):
         temperature = input('Please enter a numeric value for temperature value or enter \'f\' to end. >>> ')
     # If user enters f, we continue. The while above will exit the loop and results displayed
     if temperature.lower() == 'f':
         continue
     else:
         # Add temperature value to the list
-        temperatures.append(temperature)
-print("List before the sort " + str(temperatures))
-# The sort operation on the list affects the list permanently
-temperatures.sort()
-print("List after the sort " + str(temperatures))
-print("....................")
-print("....................")
-print('Number of temperatures entered: ' + str(len(temperatures)))
-print('Max Temperature:    ' + str(temperatures[-1]))
-print('Min Temperature:    ' + str(temperatures[0]))
-print("....................")
-print("....................")
+        if temperature.isnumeric():
+            # store as int if type is int
+            temperatures.append(int(temperature))
+        elif isfloat(temperature):
+            # store as float if type is float
+            temperatures.append(float(temperature))
+        elif isnegative(temperature):
+            # store negative number
+             temperatures.append(float(temperature))
+
+if len(temperatures) > 0:
+    print("....................")
+    print("....................")
+    print("List of numbers" + str(temperatures))
+    print('Number of temperatures entered: ' + str(len(temperatures)))
+    print('Max Temperature:    ' + str(getmax()))
+    print('Min Temperature:    ' + str(getmin()))
+    print("....................")
+    print("....................")
+
 # Exit the program
 print("Thank You. Please come back.")
