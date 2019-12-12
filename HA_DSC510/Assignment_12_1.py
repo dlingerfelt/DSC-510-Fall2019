e9bf6828a11cee39ff3e396db2dd85029ac9e65f
ju7549 <jubyung.ha@baies.org>
2019-12-11 23:17:44 -0500

    For assignments

---
+++ HA_DSC510/Assignment_12_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# File: Assignment_12_1.py
+# Name: Jubyung Ha
+# Date: 12/14/2019
+# Course: DSC510-T303 Introduction to Programming (2203-1)
+
+# Desc:
+# For your class project we will be creating an application to interacts with a webservice in order to obtain data.
+# Your program will use all of the information you’ve learned in the class in order to create a useful application.
+# Your program must prompt the user for their city or zip code and request weather forecast data from OpenWeatherMap.
+# Your program must display the weather information in a READABLE format to the user.
+
+# Requirements:
+# Create a header for your program just as you have in the past.
+# Create a Python Application which asks the user for their zip code or city.
+# Use the zip code or city name in order to obtain weather forecast data from OpenWeatherMap.
+# Display the weather forecast in a readable format to the user.
+# Use comments within the application where appropriate in order to document what the program is doing.
+# Use functions including a main function.
+# Allow the user to run the program multiple times to allow them to look up weather conditions for multiple locations.
+# Validate whether the user entered valid data. If valid data isn’t presented notify the user.
+# Use the Requests library in order to request data from the webservice.
+# Use Try blocks to ensure that your request was successful.
+# If the connection was not successful display a message to the user.
+# Use Python 3
+# Use try blocks when establishing connections to the webservice.
+# You must print a message to the user indicating whether or not the connection was successful
+
+# Usage:
+
