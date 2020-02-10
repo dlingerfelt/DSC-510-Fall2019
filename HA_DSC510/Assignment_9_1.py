3b32b301ff3948d7f2d4de8b00feed68621fa7bd
ju7549 <jubyung.ha@baies.org>
2020-02-09 21:28:39 -0500

    For assignments

--- HA_DSC510/Assignment_9_1.py
+++ HA_DSC510/Assignment_9_1.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 
-# File: Assignment_8_1.py
+# File: Assignment_9_1.py
 # Name: Jubyung Ha
-# Date: 01/20/2020
+# Date: 02/09/2020
 # Course: DSC510-T303 Introduction to Programming (2203-1)
 
-# Desc: For this week we will modify our Gettysburg processing program from week 8 in order to generate a text file from
-# the output rather than printing to the screen. Your program should have a new function called process_file
-# which prints to the file (this method should almost be the same as the pretty_print function from last week.
+# Desc: Last week we got a taste of working with files.
+# This week we’ll really dive into files by opening and closing files properly.
+#
+# For this week we will modify our Gettysburg processing program from week 8
+# in order to generate a text file from the output rather than printing to the screen.
+# Your program should have a new function called process_file which prints to the file
+# (this method should almost be the same as the pretty_print function from last week.)
 # Keep in mind that we have print statements in main as well.
 # Your program must modify the print statements from main as well.
+#
+# Your program must have a header. Use the programming style guide for guidance.
+# Create a new function called process_fie.
+# This function will perform the same operations as pretty_print from week 8
+# however it will print to a file instead of to the screen.
+# Modify your main method to print the length of the dictionary to the file as opposed to the screen.
+# This will require that you open the file twice. Once in main and once in process_file.
+# Prompt the user for the filename they wish to use to generate the report.
+# Use the filename specified by the user to write the file.
+# This will require you to pass the file as an additional parameter to your new process_file function.
 #
 # Your program must have a header. Use the programming style guide for guidance.
 # Create a new function called process_fie.
@@ -50,7 +64,7 @@ def Process_line(line, summary):
         No return value.
     """
     line = line.lower()  # lower all words in the line
-    line = line.translate(str.maketrans('','', string.punctuation))  # Remove all punctuation
+    line = line.translate(str.maketrans('', '', string.punctuation))  # Remove all punctuation
     words = line.split()  # Split word from the line
     add_word(words, summary)  # Add word to dictionary
 
@@ -62,13 +76,26 @@ def Pretty_print(summary):
     Returns:
          No return value.
     """
-    print('Length of the dictionary: {}'.format(len(summary)))
     print('Word', '              ', 'Count')
     print('-------------------------')
     # Sort the dictionary by value
     for word, count in sorted(summary.items(), key=lambda kv: kv[1], reverse=True):
         print("{:17} {:5}".format(word, count))
 
+
+def process_file(summary, txt_file):
+    """Print to the file
+    Args:
+        summary: dictionary
+        txt_file: file object
+    :return:
+        None
+    """
+    txt_file.write('Word', '              ', 'Count')
+    txt_file.write('-------------------------')
+    # Sort the dictionary by value
+    for word, count in sorted(summary.items(), key=lambda kv: kv[1], reverse=True):
+        txt_file.write("{:17} {:5}".format(word, count))
 
 def main():
     """it will open the file and call process_line on each line.
@@ -78,7 +105,13 @@ def main():
     gba_file = open('gettysburg.txt', 'r')
     for line in gba_file:
         Process_line(line, summary)
-    Pretty_print(summary)
+    file_name = input('Please enter file name: ')
+    file_path = "{}.txt".format(file_name)
+
+    txt_file = open(file_path, 'w')
+    txt_file.write('Length of the dictionary: {}'.format(len(summary)))
+
+    process_file(summary, txt_file)
 
 
 if __name__ == '__main__':
